07b3183beefc6d9a79c9ec1c4f51687a453cc229
Luca <luca.koudsi@gmail.com>
2026-06-22 11:59:37 +0200

    Fix opentelemetry build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        '@opentelemetry/api': false,
+      };
+    }
+    return config;
+  },
   experimental: {
-    serverComponentsExternalPackages: ['sequelize', 'sqlite3', 'openai', '@opentelemetry/api'],
+    serverComponentsExternalPackages: ['sequelize', 'sqlite3', 'openai', '@opentelemetry/api', '@supabase/supabase-js'],
   },
 };
 
